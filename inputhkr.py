63c95594cd8385f15a9f95b98459b9aab8e7392b
raf <manh.bv191946@sis.hust.edu.vn>
2022-07-02 20:38:58 +0700

    them bai

--- inputhkr.py
+++ inputhkr.py
@@ -1,3 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+def breakingRecords(scores):
+    # Write your code here
+    print("test")
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
